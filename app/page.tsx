7f8ba738569a90d462b462e754b215c0c1bad3f1
hamanamidesign <yujihamaue@hamanami.design>
2026-03-15 12:36:46 +0900

    写真枠の調整

--- app/page.tsx
+++ app/page.tsx
@@ -849,7 +849,7 @@ const resetKarteFields = () => {
 {/* 今回の写真撮影エリア */}
 <div className="flex-1 p-2 overflow-y-auto bg-blue-50/10">
 
-  <div className="text-center text-[10px] font-black text-blue-700 mb-2 border-b">
+  <div className="text-center text-[10px] font-black text-blue-700 mb-2">
     今回の点検写真
   </div>
 
